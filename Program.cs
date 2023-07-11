0978b5f745e42f0ae76a301ac8dec2c249b97c70
harshal43 <harshal88843bh@gmail.com>
2023-07-11 12:13:13 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppMethod
+{
+    public class Emp
+    {
+        public string Fullname(string fName, string lName)
+        {
+            return fName +" "+ lName;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp emp = new Emp();
+            Console.WriteLine(emp.Fullname("harshal", "bharatkar"));
+            Console.ReadKey();
+        }
+    }
+}
